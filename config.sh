99019fc7a3c0280ecd7c53f65199ba78be503987
manikandan59131 <55307264+manikandan59131@users.noreply.github.com>
2022-01-10 11:02:06 +0530

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,6 @@
+readonly DOCKER_IMAGE_NAME="jackwilsdon/esp8266"
+readonly DOCKER_IMAGE_TAG="0.1.0"
+readonly DOCKER_IMAGE_MOUNT="/mnt/data"
+
+readonly DOCKER_LOCAL_SOURCE_DIRECTORY="docker"
+readonly DOCKER_LOCAL_MOUNT_DIRECTORY="data"
